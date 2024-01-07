4ee28565ccd91c1716e7c1d9dc4125dbd7a2e16c
dinesh97-developer <148607532+dinesh97-developer@users.noreply.github.com>
2024-01-07 12:24:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var f1 = fetch("https://restcountries.com/v3.1/all")
+f1.then((data)=>data.json()).then((data1)=>foo(data1));
+
+
+/* var f1 = fetch("https://data.covid19india.org/v4/min/data.min.json")
+f1.then((data)=>data.json()).then((data1)=>console.log(data1)); */
+
+var container = document.createElement("div");
+container.className = "container"
+
+var row =  document.createElement("div");
+row.className = "row"
+
+function foo(data1){
+    console.log(data1);
+    for(i=0;i<data1.length;i++)
+    {
+        var col =document.createElement("div");
+        col.className = "col-md-4"
+        col.innerHTML += 
+        `<div class="card" style="width: 18rem;">
+        <img src = "${data1[i].flags.png}" class="card-img-top" alt ="...">
+        <div class = "card-body">
+        <p class="card-text"><b>Country Name:</b> "${data1[i].name.common}"
+        <br><b>Capital:</b>"${data1[i].capital}"
+        </p>
+        </div>
+        </div>`
+    row.append(col);
+    container.append(row);
+    document.body.append(container);
+    }
+    
+}
